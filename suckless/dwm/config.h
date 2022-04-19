4598552f1d3fd6791365deb1b92d20dbdf9a414f
crash-exe <81610798+crash-exe@users.noreply.github.com>
2022-04-19 11:09:37 +0200

    My changes to the config.h file in suckless dwm
    These changes allow emojis (and fix the layout a bit)
    Replace these lines in your configure.h file

---
+++ suckless/dwm/config.h
@@ -0,0 +1,10 @@
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Monospace Color Emoji:pixelsize=16:antialias=true:autohint=true" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#ff0000";
+static const char col_gray3[]       = "#008800";
+static const char col_gray4[]       = "#ffff00";
+static const char col_cyan[]        = "#444400";
